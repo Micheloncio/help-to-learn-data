d2d88e048244d9223c732e5b24255b813f00f0e7
Micheloncio <escolano_garnica@hotmail.com>
2018-01-24 15:07:55 +0100

    add WordData

---
+++ word/WordData.js
@@ -0,0 +1,32 @@
+const Word = require('./WordModel')
+
+class WordData {
+
+    listWords(){
+        return new Promise((resolve, reject) => {
+            Word.find({})
+                .then(resolve)
+                .catch(reject)
+        })  
+    }
+    addWord(english, spanish, type){
+        return new Promise((resolve, reject) => {
+            if (!english)
+                throw new Error('no english word provided')
+
+            if (!spanish)
+                throw new Error('no spanish word provided')
+
+            if (!type)
+                throw new Error('no type word provided')
+
+            const word = new Word({ english, spanish, type })
+
+            word.save()
+                .then(resolve)
+                .catch(reject)
+        })  
+    }
+}
+
+module.exports = WordData
